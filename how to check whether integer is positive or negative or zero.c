4f985264d3ce79f98697d24c16fb4d87a8d4c627
DEVAKUMAR123 <52273073+DEVAKUMAR123@users.noreply.github.com>
2019-07-26 14:25:36 +0530

    Add files via upload

---
+++ how to check whether integer is positive or negative or zero.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+int a;
+
+printf("Enter the number:");
+
+scanf("%d",&a);
+
+if(a>0)
+{
+	
+printf("Positive");
+
+}
+
+else if(a<0)
+
+{
+	
+printf("Negative");
+
+}
+
+else
+
+{
+	
+printf("zero");
+
+}
+	
+return 0;
+
+}
