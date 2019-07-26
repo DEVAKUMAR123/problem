0498b59d885e32fd7b25a3af194fe4f576705310
DEVAKUMAR123 <52273073+DEVAKUMAR123@users.noreply.github.com>
2019-07-26 14:36:10 +0530

    Add files via upload

---
+++ number is even or odd or invalid.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+	
+int a;
+	
+printf("Enter the number:");
+	
+scanf("%d",&a);
+	
+	
+if(a%2==0)
+	
+{
+		
+printf("Even number");
+	
+}
+	
+else if(a%2==1)
+
+	{
+		
+printf("Odd number");
+
+	}
+	
+else
+	
+{
+		
+printf("Invalid number");
+	
+}
+	
+return 0;
+
+}
+
